8f4a580342c0eb75b17a5c45f66fb80b5ebb4ef9
wojbog <wojtek44wojciech@gmail.com>
2024-05-13 23:27:24 +0200

    get, post scenario controller

--- src/main/java/pl/put/poznan/transformer/logic/Scenario.java
+++ src/main/java/pl/put/poznan/transformer/logic/Scenario.java
@@ -1,4 +1,40 @@
 package pl.put.poznan.transformer.logic;
 
+import java.util.Arrays;
+
 public class Scenario {
+    public String title;
+
+    public String[] actors;
+
+    public String systemActor;
+
+    public Step[] steps;
+    public Scenario(String title, String[] actors, String systemActor, Step[] steps) {
+        this.title = title;
+        this.actors = actors;
+        this.systemActor = systemActor;
+        this.steps = steps;
+    }
+
+    @Override
+    public String toString() {
+        String steps = "";
+        if (this.steps != null) {
+            steps = "{";
+            for (Step step : this.steps) {
+                steps += step.toString();
+                steps += ", ";
+            }
+            steps += "}";
+        }
+        return "Scenario{" +
+                "title='" + title + '\'' +
+                ", actors=" + Arrays.toString(actors) +
+                ", systemActor='" + systemActor + '\'' +
+                ", steps=" + steps +
+                '}';
+    }
+
+
 }
--- src/main/java/pl/put/poznan/transformer/rest/ScenarioController.java
+++ src/main/java/pl/put/poznan/transformer/rest/ScenarioController.java
@@ -1,4 +1,24 @@
 package pl.put.poznan.transformer.rest;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import pl.put.poznan.transformer.logic.Scenario;
+
+@RestController
+@RequestMapping("/scenario")
 public class ScenarioController {
+
+    public Scenario scenario;
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    public Scenario get(){
+        return this.scenario;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    public void post(@RequestBody Scenario scenario){
+        this.scenario = scenario;
+    }
 }
