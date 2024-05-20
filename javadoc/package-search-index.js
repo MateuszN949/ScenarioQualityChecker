aa15560d9ec0803dc5aebda59e4cb1a9f26ab003
MateuszN949 <150344537+MateuszN949@users.noreply.github.com>
2024-05-20 20:18:29 +0000

    Deploying to javadoc from @ MateuszN949/ScenarioQualityChecker@e5ffdfa7e6f8db7e869149302d37117713ebdd3b 🚀

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"pl.put.poznan.transformer.app"},{"l":"pl.put.poznan.transformer.logic"},{"l":"pl.put.poznan.transformer.rest"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"pl.put.poznan.transformer.app"},{"l":"pl.put.poznan.transformer.logic"},{"l":"pl.put.poznan.transformer.rest"}];updateSearchResults();
