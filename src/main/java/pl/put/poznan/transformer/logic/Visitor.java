9a844c83dc40827a914ae0c5c3e9c14732f0356a
MateuszN949 <mateusz.nowicki@onet.eu>
2024-05-21 14:41:07 +0200

    Fix documentation in Visitor interface

--- src/main/java/pl/put/poznan/transformer/logic/Visitor.java
+++ src/main/java/pl/put/poznan/transformer/logic/Visitor.java
@@ -1,15 +1,15 @@
 package pl.put.poznan.transformer.logic;
 
-/*
-    * This is the Visitor interface.
-    * It is used to implement the Visitor design pattern.
-    * The visitor visits the scenario and performs some operations on it.
+/**
+ * This is the Visitor interface.
+ * It is used to implement the Visitor design pattern.
+ * The visitor visits the scenario and performs some operations on it.
  */
 public interface Visitor {
-    /*
-        * This is the Visit method.
-        * It is used to visit the scenario and perform some operations on it.
-        * @param scenario This is the scenario that the visitor visits.
+    /**
+     * This is the Visit method.
+     * It is used to visit the scenario and perform some operations on it.
+     * @param scenario This is the scenario that the visitor visits.
      */
     public void Visit(Scenario scenario);
 }
