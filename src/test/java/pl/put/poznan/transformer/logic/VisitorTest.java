efca176ce4de9633e007ee4af8678863abb9e016
MateuszN949 <mateusz.nowicki@onet.eu>
2024-06-10 19:37:25 +0200

    Add remaining tests

--- src/test/java/pl/put/poznan/transformer/logic/VisitorTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/VisitorTest.java
@@ -3,11 +3,40 @@ package pl.put.poznan.transformer.logic;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 class VisitorTest {
     private StepsCounter stepsCounter;
     private ConditionalDecisionsCounter conditionalDecisionsCounter;
     private StepsPruner stepsPruner;
     private Scenario scenario;
+
+    @Test
+    public void testVisitCounter() throws CloneNotSupportedException {
+        StepsCounter mock = mock(StepsCounter.class);
+        scenario = new Scenario(null, null, null, null);
+
+        scenario.Accept(mock);
+        verify(mock, times(1)).Visit(any(Scenario.class));
+    }
+
+    @Test
+    public void testVisitConditions() throws CloneNotSupportedException {
+        ConditionalDecisionsCounter mock = mock(ConditionalDecisionsCounter.class);
+        scenario = new Scenario(null, null, null, null);
+
+        scenario.Accept(mock);
+        verify(mock, times(1)).Visit(any(Scenario.class));
+    }
+
+    @Test
+    public void testVisitPruner() throws CloneNotSupportedException {
+        StepsPruner mock = mock(StepsPruner.class);
+        scenario = new Scenario(null, null, null, null);
+
+        scenario.Accept(mock);
+        verify(mock, times(1)).Visit(any(Scenario.class));
+    }
 
     @Test
     public void testCounting() throws CloneNotSupportedException {
@@ -37,6 +66,17 @@ class VisitorTest {
     }
 
     @Test
+    public void testCompletePruner() throws CloneNotSupportedException {
+        stepsPruner = new StepsPruner(0);
+
+        Step step0 = new Step(null, null, null);
+        scenario = new Scenario(null, null, null, new Step[]{step0});
+
+        scenario.Accept(stepsPruner);
+        assertNull(stepsPruner.getResult().steps);
+    }
+
+    @Test
     public void testPruner() throws CloneNotSupportedException {
         stepsPruner = new StepsPruner(1);
 
@@ -44,9 +84,9 @@ class VisitorTest {
         Step step1 = new Step(null, null, new Step[]{step0});
         Step step2 = new Step(null, null, new Step[]{step1, step0});
         Step step3 = new Step(null, null, new Step[]{step2, step1});
-        scenario = new Scenario("ELSE", null, null, new Step[]{step0, step1, step2, step3});
+        scenario = new Scenario(null, null, null, new Step[]{step0, step1, step2, step3});
 
         scenario.Accept(stepsPruner);
-        assertEquals(null, stepsPruner.getResult().steps[3].steps);
+        assertNull(stepsPruner.getResult().steps[3].steps);
     }
 }
